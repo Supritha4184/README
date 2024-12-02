486df1784ab6d58ae1cee16345e576cdcafed870
Supritha4184 <supritha932@gmail.com>
2024-12-02 14:12:35 +0530

    Create registration.py

---
+++ registration.py
@@ -0,0 +1,6 @@
+class Registration:
+    def __init__(self, name, email, date_of_birth, phone_number=None):
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.phone_number = phone_number
